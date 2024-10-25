ba5105813b45ca527bdaaa0be76b2622748688ab
Lei <linhaibin676@gmail.com>
2024-10-25 19:18:53 +0800

    close工具

---
+++ cls/close.go
@@ -0,0 +1,50 @@
+package cls
+
+import "sync"
+
+type CloseUtil struct {
+	closeChan chan struct{}
+	once      sync.Once
+	closeCb   []func()
+}
+
+func NewCloseUtil() *CloseUtil {
+	return &CloseUtil{
+		closeChan: make(chan struct{}),
+	}
+}
+
+func MakeCloseUtil() CloseUtil {
+	return CloseUtil{
+		closeChan: make(chan struct{}),
+	}
+}
+
+func (c *CloseUtil) IsClosed() bool {
+	select {
+	case <-c.closeChan:
+		return true
+	default:
+	}
+	return false
+}
+
+func (c *CloseUtil) C() <-chan struct{} {
+	return c.closeChan
+}
+
+func (c *CloseUtil) Close() {
+	c.once.Do(func() {
+		close(c.closeChan)
+		for _, f := range c.closeCb {
+			f()
+		}
+	})
+}
+
+func (c *CloseUtil) RegisterCloseCallback(f func()) {
+	if f == nil {
+		return
+	}
+	c.closeCb = append(c.closeCb, f)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Lei2050/lei-utils
+
+go 1.22.1
